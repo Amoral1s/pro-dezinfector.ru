70f98c91b2b68070e298b74bf9606c99d38d84a9
Dmitriy Filippov <Amoralis.19@gmail.com>
2020-11-30 15:07:49 +0300

    1

--- js/common.js
+++ js/common.js
@@ -10,4 +10,32 @@ jQuery(document).ready(function () {
       jQuery('.header__menu').slideToggle(150);
     });
   }
+
+  jQuery('.pdf-trigger').on('click', function() {
+    jQuery('.overlay').fadeIn(200);
+    jQuery('.popup-pdf').fadeIn(200);
+  });
+
+  jQuery('.goods-trigger').on('click', function() {
+    jQuery('.overlay').fadeIn(200);
+    jQuery('.popup-goods').fadeIn(200);
+    var text = jQuery(this).prev().prev().prev().text() + ' ' + jQuery(this).prev().prev().text();
+
+    jQuery('.popup-goods strong b').text(text);
+    jQuery('.popup-goods input.hidden').val(text);
+  });
+  jQuery('.call-trigger').on('click', function() {
+    jQuery('.overlay').fadeIn(200);
+    jQuery('.popup-call').fadeIn(200);
+  });
+
+  jQuery('.close').on('click', function() {
+    jQuery('.overlay').fadeOut(200);
+    jQuery('.popup').fadeOut(200);
+  });
+  jQuery('.overlay').on('click', function() {
+    jQuery('.overlay').fadeOut(200);
+    jQuery('.popup').fadeOut(200);
+  });
+
 });
